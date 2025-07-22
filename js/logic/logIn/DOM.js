8e94002da6927dc5f0e64a6adb72213012cd3141
kwangche <163306559+kwangche@users.noreply.github.com>
2025-07-22 19:02:25 +0900

    로그인 유효성 검증 함수 추가

---
+++ js/logic/logIn/DOM.js
@@ -0,0 +1,5 @@
+
+const form = document.querySelector('.form-box'); // 로그인, 회원가입 폼
+const emailInput = form.querySelector('#email'); // 이메일 입력창
+const passwordInput = form.querySelector('#password'); // 비밀번호 입력창
+const btnForm = form.querySelector('.form-btn'); // 로그인, 회원가입 버튼
