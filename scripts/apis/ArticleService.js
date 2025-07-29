937157d3b19f6a9bdecf07754ed4db59968f1963
Dongyoung.Kim <cuckooolly@gmail.com>
2025-07-29 13:52:04 +0900

    Reapply "Reapply "fix: Change asynchronization methods from keywords to using Promise objects.""
    This reverts commit 7330a0240d6e727134ad49f93a8b6c96327a69da.

--- scripts/apis/ArticleService.js
+++ scripts/apis/ArticleService.js
@@ -14,38 +14,32 @@ async function getArticleList(article_list_data) {
     const params = new URLSearchParams(article_list_data);
     url.search = params.toString();
 
-    try{
-        const response = await fetch(url);
+    const response = await fetch(url)
+        .then(response => {
+            if (!response.ok){
+                console.error(response.status)
+            }
+            return response.json()
+        })
+        .catch(error => console.error(error))
 
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+    return response;
 }
 
 // Article 목록을 가져오는 함수
 async function getArticle() {
     const url = new URL("https://panda-market-api-crud.vercel.app/articles");
     
-    try{
-        const response = await fetch(url);
+    const response = await fetch(url)
+        .then(response => {
+            if (!response.ok){
+                console.error(response.status)
+            }
+            return response.json()
+        })
+        .catch(error => console.error(error))
 
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+    return response;
 }
 
 // 사용자 입력 데이터에 따라 Article을 생성하는 함수
@@ -60,71 +54,59 @@ async function createArticle(article_data) {
     
     const url = new URL("https://panda-market-api-crud.vercel.app/articles");
 
-    try{
-        const response = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(article_data),
-            headers: {'Content-Type': 'application/json'}
-        });
-
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-
-        const response_result = await response.json();
-        return response_result;
-
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(article_data),
+        headers: {'Content-Type': 'application/json'}
+    })
+        .then(response => {
+            if (!response.ok){
+                console.error(response.status)
+            }
+            return response.json()
+        })
+        .catch(error => console.error(error))
+    
+    return response;
 }
 
 // 사용자 입력 데이터에 따라 Article을 수정하는 함수
 async function patchArticle(article_id, patch_data) {
     const url = new URL(`https://panda-market-api-crud.vercel.app/articles/${article_id}`);
 
-    try{
-        const response = await fetch(url, {
-            method: 'PATCH',
-            body: JSON.stringify(patch_data),
-            headers: {'Content-Type': 'application/json'}
-        });
+    const response = await fetch(url, {
+        method: 'PATCH',
+        body: JSON.stringify(patch_data),
+        headers: {'Content-Type': 'application/json'}
+    })
+        .then(response => {
+            if (!response.ok){
+                console.error(response.status)
+            }
+            return response.json()
+        })
+        .catch(error => console.error(error))
 
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-
-        const response_result = await response.json();
-        return response_result;
-
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+    return response;
 }
 
 // Article을 삭제하는 함수
 async function deleteArticle(article_id) {
     const url = new URL(`https://panda-market-api-crud.vercel.app/articles/${article_id}`);
     
-    try{
-        const response = await fetch(url, {
-            method: 'DELETE',
-            headers: {'Content-Type': 'application/json'}
-        });
-
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-
-        const response_result = await response.json();
-        return response_result;
-
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+    const response = await fetch(url, {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'}
+    })
+        .then(response => {
+            if (!response.ok){
+                console.error(response.status)
+            }
+            return response.json()
+        })
+        .catch(error => console.error(error))
+    
+    return response
 }
 
 
