708edd47b8fcd77daa85cecfbff976c5de255d45
MyeongjuHong <hmjpearl@gmail.com>
2025-08-04 00:47:26 +0900

    feat: Set up structure in main.js and test.html

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import {
+  getArticleList,
+  getArticle,
+  createArticle,
+  patchArticle,
+  deleteArticle,
+} from "./ArticleService";
+import {
+  getProductList,
+  getProduct,
+  createProduct,
+  patchProduct,
+  deleteProduct,
+} from "./ProductService";
