81ac4fea039328e458c0c69d16395819a6b3f68c
MyeongjuHong <hmjpearl@gmail.com>
2025-08-03 23:57:10 +0900

    feat: Add getArticleList API logic

--- js/ArticleService.js
+++ js/ArticleService.js
@@ -1,6 +1,21 @@
 // Article
 
-export async function getArticleList() {}
+const url = new URL(`https://panda-market-api-crud.vercel.app/products`);
+
+export async function getArticleList(params = {}) {
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("데이터를 불러오는데 실패했습니다.");
+  }
+
+  const data = await res.json();
+  return data;
+}
 
 export async function getArticle() {}
 
