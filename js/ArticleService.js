5c723c67b4cc2c9c98250af964a901c087a73d41
MyeongjuHong <hmjpearl@gmail.com>
2025-08-03 23:56:04 +0900

    feat: Set up base for API functions

---
+++ js/ArticleService.js
@@ -0,0 +1,11 @@
+// Article
+
+export async function getArticleList() {}
+
+export async function getArticle() {}
+
+export async function createArticle() {}
+
+export async function patchArticle() {}
+
+export async function deleteArticle() {}
---
+++ js/ProductService.js
@@ -0,0 +1,10 @@
+// Product
+export async function getProductList() {}
+
+export async function getProduct() {}
+
+export async function createProduct() {}
+
+export async function patchProduct() {}
+
+export async function deleteProduct() {}
