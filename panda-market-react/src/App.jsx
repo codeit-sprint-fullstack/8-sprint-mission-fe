34b3ad30017054f000271755d59ac0b6bce04bcb
seongEun95 <dsa02070@naver.com>
2025-08-05 18:38:41 +0900

    feat: 라우터 설정

--- panda-market-react/src/App.jsx
+++ panda-market-react/src/App.jsx
@@ -1,16 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import { ProductListPage } from './pages/ProductListPage'
+import { Route, Routes } from 'react-router-dom';
+import { ProductListPage } from './pages/ProductListPage';
+import { Layout } from './components/layout/Layout';
 
 function App() {
-
   return (
     <>
-      <h1>판다마켓</h1>
       <Routes>
-        <Route path="/" element={<ProductListPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<ProductListPage />} />
+        </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
