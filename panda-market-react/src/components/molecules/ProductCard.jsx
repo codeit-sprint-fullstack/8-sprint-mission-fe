62b602b52594c7d754f7d84bf4d98c809bf3b10c
seongEun95 <dsa02070@naver.com>
2025-08-06 13:28:02 +0900

    feat: 좋아요버튼, 카드, 타이틀 컴포넌트 추가

---
+++ panda-market-react/src/components/molecules/ProductCard.jsx
@@ -0,0 +1,24 @@
+import { LikeButton } from '../atoms/LikeButton';
+import styles from '../../styles/components/molecules/ProductCard.module.css';
+import { Link } from 'react-router-dom';
+
+export function ProductCard({
+  id,
+  name,
+  price,
+  image = '/product-list/prod-test.png',
+  favoriteCount,
+}) {
+  return (
+    <div className={styles.productCard}>
+      <Link to={`/products/${id}`}>
+        <img src={image} alt={name} />
+        <div className={styles.productCardContent}>
+          <h2>{name}</h2>
+          <p>{price.toLocaleString()}원</p>
+          <LikeButton favoriteCount={favoriteCount} />
+        </div>
+      </Link>
+    </div>
+  );
+}
