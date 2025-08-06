1fa6853c86837693999a85a544ac25635d0701e1
Dongyoung.Kim <cuckooolly@gmail.com>
2025-08-06 18:49:06 +0900

    chore: Implement of ProductCard component.

---
+++ components/ProductCard.jsx
@@ -0,0 +1,22 @@
+import "/styles/components/ProductCard.css";
+
+function ProductCard({item}) {
+    const { images, description, name, price, favoriteCount } = item;
+
+    return (
+        <div className="product-card">
+            <img src={images[0]} alt={description} className="product-image"/>
+            <div className="padding-space"></div>
+            <div className="product-details">
+                <p className="product-name">{name}</p>
+                <h3 className="product-price">{price.toLocaleString()}원</h3>
+                <div className="product-likes">
+                    <img src={"images/icons/ic_heart.svg"} className="product-likes-image" alt="likes"></img>
+                    <p className="product-likes-text">{favoriteCount}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
