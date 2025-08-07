5367256b63e0767060c07c3ccbf19ea89082a53b
seongEun95 <dsa02070@naver.com>
2025-08-07 20:31:07 +0900

    feat: 가로 너비 구하기 & 드랍다운 외부 클릭 시 닫는 커스텀 훅 추가

---
+++ panda-market-react/src/lib/hooks/useWindowWidth.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export function useWindowWidth(delay = 500) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    let timeoutId;
+    const handleResize = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setWindowWidth(window.innerWidth);
+      }, delay);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      clearTimeout(timeoutId);
+    };
+  }, [delay]);
+
+  return windowWidth;
+}
