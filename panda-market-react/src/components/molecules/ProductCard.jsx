bcc1bb0697b84b6ebbec8189a5ba320164ece852
seongEun95 <dsa02070@naver.com>
2025-08-08 11:48:57 +0900

    fix: 이미지 엑스박스 에러 처리 (기본 이미지 경로 값 추가)

--- panda-market-react/src/components/molecules/ProductCard.jsx
+++ panda-market-react/src/components/molecules/ProductCard.jsx
@@ -2,17 +2,17 @@ import { LikeButton } from '../atoms/LikeButton';
 import styles from '../../styles/components/molecules/ProductCard.module.css';
 import { Link } from 'react-router-dom';
 
-export function ProductCard({
-  id,
-  name,
-  price,
-  image = '/product-list/prod-test.png',
-  favoriteCount,
-}) {
+const imgReplaceUrl = '/product-list/prod-test.png';
+
+export function ProductCard({ id, name, price, image = imgReplaceUrl, favoriteCount }) {
+  function handleImageError(e) {
+    e.currentTarget.src = imgReplaceUrl;
+  }
+
   return (
     <div className={styles.productCard}>
       <Link to={`/products/${id}`}>
-        <img src={image} alt={name} />
+        <img src={image} alt={name} onError={handleImageError} />
         <div className={styles.productCardContent}>
           <h2>{name}</h2>
           <p>{price.toLocaleString()}원</p>
