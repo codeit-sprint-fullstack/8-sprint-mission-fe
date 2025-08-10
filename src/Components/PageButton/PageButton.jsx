7299bcc5864d725df747b7b121a82748b4d83bf4
chaewon127 <osobi621@gmail.com>
2025-08-10 18:01:41 +0900

    PageButton 작성

--- src/Components/PageButton/PageButton.jsx
+++ src/Components/PageButton/PageButton.jsx
@@ -4,16 +4,20 @@ import style from './PageButton.module.css';
 import ic_arrow_left from './ic_PageButton_left.svg';
 import ic_arrow_right from './ic_PageButton_right.svg';
 
-function PageButton() {
-  
+function PageButton({ nowPage = 1, buttonLength = 5, pageSize = 10, totalCount = 50, onChange = null }) {
+  const startPage = Math.floor((nowPage - 1) / buttonLength) * buttonLength + 1;
+  const maxPage = Math.ceil(totalCount / pageSize);
+
+  const pages = Array.from({ length: buttonLength }, (_, i) => startPage + i)  // _는 콜백 함수 매개변수 중 첫 번째(값)는 무시하고, 두 번째(인덱스)를 사용하겠다는 의미
+                      .filter(pageNum => pageNum <= maxPage);
 
   return (
-    <div className="PageButtons">
+    <div className={style.PageButtons}>
       <button
         onClick={() => onChange?.(nowPage - 1)}
-        disabled={nowPage <= 1}
+        disabled={nowPage <= 1}  // 첫 번째 페이지에서는 이전 버튼 비활성화
       >
-        <img src={ic_arrow_left} />
+        <img src={ic_arrow_left} alt='왼쪽 화살표' />
       </button>
 
       {pages.map((pageNum) => {
@@ -31,9 +35,10 @@ function PageButton() {
 
       <button
         onClick={() => onChange?.(nowPage + 1)}
-        disabled={nowPage >= maxPage}
+        disabled={nowPage >= maxPage}  // 마지막 페이지에서는 다음 버튼 비활성화
       >
-        <img src={ic_arrow_right} />
+        <img src={ic_arrow_right} alt='오른쪽 화살표' />
+        {/* CSS로 화살표 좌우반전 가능하대! */}
       </button>
     </div>
   );
