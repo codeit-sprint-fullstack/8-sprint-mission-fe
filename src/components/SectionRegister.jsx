1656c9a9bbc034be7b20f1e3445176ed0a040dce
sungminiioo <sungminiioo426@gmail.com>
2025-08-19 18:37:26 +0900

    SectionRegister 컴포넌트 생성

---
+++ src/components/SectionRegister.jsx
@@ -0,0 +1,16 @@
+export default function SectionRegister() {
+  return (
+    <section className="section-register">
+      <div className="container section-register-content">
+        <img src="/assets/images/register.svg" alt="register"/>
+        <div>
+          <div className="register-text">Register</div>
+          <div className="register-title">판매를 원하는<br/>상품을 등록하세요</div>
+          <div className="register-subtitle">
+            어떤 물건이든 판매하고 싶은 상품을<br/>쉽게 등록하세요
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
