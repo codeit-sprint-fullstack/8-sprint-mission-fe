457ff50f3a178eda08b3f05ee1547f45679e0d39
sungminiioo <sungminiioo426@gmail.com>
2025-08-19 18:36:32 +0900

    SectionSearch 컴포넌트 생성

---
+++ src/components/SectionSearch.jsx
@@ -0,0 +1,16 @@
+export default function SectionSearch() {
+  return (
+    <section className="section-search">
+      <div className="container section-search-content">
+        <div>
+          <div className="search-text">Search</div>
+          <div className="title-text">구매를 원하는<br/>상품을 검색하세요</div>
+          <div className="subtitle-text">
+            구매하고 싶은 물품을 검색해서<br/>쉽게 찾아보세요
+          </div>
+        </div>
+        <img className="search-image" src="/assets/images/search.svg" alt="search"/> 
+      </div>
+    </section>
+  );
+}
