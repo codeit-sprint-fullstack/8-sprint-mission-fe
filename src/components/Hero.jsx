15b079fd11ba32d00db03d58ae0da295da0825a1
sungminiioo <sungminiioo426@gmail.com>
2025-08-19 18:34:09 +0900

    Hero 컴포넌트 생성

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div className="top">
+      <div className="container hero-content">
+        <div className="hero-text-group">
+          <div className="hero-title">
+            일상의 모든 물건을<br />거래해 보세요
+          </div>
+          <a href="/items.html" className="hero-cta">
+            구경하러 가기
+          </a>
+        </div>
+        <div className="hero-image"></div>
+      </div>
+    </div>
+  );
+}
