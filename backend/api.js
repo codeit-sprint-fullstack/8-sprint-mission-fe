9a37f07ced8b193943cd5911644e11c81609481a
Dongyoung.Kim <cuckooolly@gmail.com>
2025-08-21 14:07:42 +0900

    feat: Add modification product data API
    - 상품 수정 API 생성
    - 각 API 코드에 주석 추가

--- backend/api.js
+++ backend/api.js
@@ -29,15 +29,46 @@ function asyncHandler(handler) {
 
 // 상품 등록 API
 router.post('/products', asyncHandler(async (req, res) => {
+    // 등록할 상품의 데이터
     const newProductData = req.body;
+
+    // 데이터를 DB에 저장
     const createProductData = await Product.create(newProductData);
+
+    // 응답으로 생성된 상품 데이터를 반환
     res.status(201).send(createProductData);
 }));
 
 // 상품 상세 조회 API
 router.get('/products/:id', asyncHandler( async (req, res) => {
+    // 조회할 상품의 ID
     const id = req.params.id;
-    const productData = await Product.find((product) => product.id === id);
 
+    // ID로 상품 데이터를 찾고, 필요한 필드만 선택
+    const productData = await Product.findById(id)
+        .select("id, name, description, price, tags, createdAt")
+        .lean();
+
+    // 선택된 상품의 데이터를 반환
     res.status(200).send(productData);
+}));
+
+// 상품 수정 API
+router.patch('/products/:id', asyncHandler(async (req, res) => {
+    // 수정할 상품의 ID와 데이터
+    const id = req.params.id;
+    const updateData = req.body;
+
+    // ID로 기존 상품 데이터를 찾음
+    const updatedProduct = await Product.findById(id);
+
+    // 상품의 데이터를 수정 및 저장
+    Object.keys(updateData).forEach((key) => {
+        updatedProduct[key] = updateData[key];
+    });
+    updatedProduct.updatedAt = new Date();
+    await updatedProduct.save();
+
+    // 수정된 상품 데이터를 응답으로 반환
+    res.status(200).send(updatedProduct);
 }));
