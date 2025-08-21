722ad55f986c6607b21df7bedaf96a8796d3db8f
DESKTOP-14A5R6M\clwns <clwns00@naver.com>
2025-08-21 19:12:03 +0900

    feat: 페이지 주소가 /items일때 버튼 색상 변경기능 추가

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import pandaLogo from "../assets/header_panda.png";
 import "../css/header.css";
 
@@ -10,8 +10,18 @@ function Header() {
           <img src={pandaLogo} alt="판다마켓 로고" />
         </Link>
         <nav className="nav-menu">
-          <Link to="/board">자유게시판</Link>
-          <Link to="/items">중고마켓</Link>
+          <Link to="/board" className="nav-item">
+            자유게시판
+          </Link>
+          {/* NavLink를 사용 함으로 현재 URL을 자동으로 비교하여 className/style 에 전달하는 콜백에 isActive를 넘겨줄 수있다 */}
+
+          <NavLink
+            to="/items"
+            end
+            className={({ isActive }) => `nav-item${isActive ? " active" : ""}`}
+          >
+            중고마켓
+          </NavLink>
         </nav>
       </div>
       <Link to="/login" className="loginButton" id="login">
@@ -20,5 +30,4 @@ function Header() {
     </header>
   );
 }
-
 export default Header;
