ece058d7dd8dc34b8739ee0f8a554085f071108e
Dongyoung.Kim <cuckooolly@gmail.com>
2025-08-21 13:38:57 +0900

    fix: Change DB schema for ID field
    - ID 필드의 type 변경 (Number -> String)
    - ID 필드의 기본값 설정 (by uuidv4())

--- backend/schema.js
+++ backend/schema.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import { v4 as uuidv4 } from "uuid";
 
 const ProductSchema = new mongoose.Schema(
     {
         id: {
-            type: Number,
+            type: String,
             required: true,
+            default: uuidv4(),
         },
         name: {
             type: String,
