82c43f4f668168dcf8216be968a08cd7f22eb7de
chaewon127 <osobi621@gmail.com>
2025-08-22 18:01:45 +0900

    input error 메세지 작성

--- src/Components/RegisterationPage/inputBoxes/inputBoxes.jsx
+++ src/Components/RegisterationPage/inputBoxes/inputBoxes.jsx
@@ -8,23 +8,27 @@ function InputBoxes() {
   return (
     <section> 
       <div className={style.container}>
-        <h2>상품명</h2>
-        <input type="text" placeholder='상품명을 입력해주세요.' />
+        <label>상품명</label>
+        <input type="text" placeholder='상품명을 입력해주세요.' maxlength="10" required />
+        <span className={style.errorMessage}>10자 이내로 입력해주세요.</span>
       </div>
 
       <div className={style.container}>
-        <h2>상품 소개</h2>
-        <input type="text" className={style.introduceInput} placeholder='상품 소개를 입력해주세요.' />
+        <label>상품 소개</label>
+        <input type="text" className={style.introduceInput} placeholder='상품 소개를 입력해주세요.' minlength="10" required />
+        <span className={style.errorMessage}>10자 이상 입력해주세요.</span>
       </div>
 
       <div className={style.container}>
-        <h2>판매가격</h2>
-        <input type="text" placeholder='판매가격을 입력해주세요.' />
+        <label>판매가격</label>
+        <input type="number" placeholder='판매가격을 입력해주세요.' />
+        <span className={style.errorMessage}>숫자로 입력해주세요.</span>
       </div>
 
       <div className={style.container}>
-        <h2>태그</h2>
-        <input type="text" placeholder='태그를 입력해주세요.' />
+        <label>태그</label>
+        <input type="text" placeholder='태그를 입력해주세요.' maxlength="5" required />
+        <span className={style.errorMessage}>5글자 이내로 입력해주세요.</span>
         <Tag />
       </div>
     </section>
