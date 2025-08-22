9e28c366745164c2fd28afcf9f5907eb9c28277b
chaewon127 <osobi621@gmail.com>
2025-08-22 14:17:59 +0900

    HeadLine 작성

---
+++ src/Components/LandingPage/Headline/Headline.jsx
@@ -0,0 +1,21 @@
+import style from './Headline.module.css';
+import HomeTopImg from '../../../images/home_top_img.svg';
+
+function Headline() {
+  return (
+    <section className={style.headlineSectionBg}>
+      <div className={style.headlineSection}>
+        <div className={style.headlineTitle}>
+          <h1>
+            일상의 모든 물건을<br />
+            거래해 보세요
+          </h1>
+          <a href="items.html" className={style.headlineButton}>구경하러 가기</a>
+        </div>
+        <img src={HomeTopImg} alt="HomeTop" className={style.headlineImage} />
+      </div>
+    </section>
+  );
+}
+
+export default Headline;
