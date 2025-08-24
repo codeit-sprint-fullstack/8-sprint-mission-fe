3fc3065769a73548b526263314001323afcf03db
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-08-24 14:22:20 +0900

    등록 버튼 비활성화 조건 / 유효성 검사 커스텀 훅 - 심화 요구사항

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import productApi from '../api/ProductService.js';
-
+import { useEffect, useState } from 'react';
 
 import '../styles/registration.css';
 
@@ -9,8 +6,9 @@ import HomeHeader from '../components/HomeHeader.jsx';
 import HomeFooter from '../components/HomeFooter.jsx';
 
 import cancleTagImg from '/images/registration/cancle_tag.svg';
+import useRegisterInput from '../components/hooks/useRegisterInput.jsx';
 
-function InputForm({label, name, value, handleChange, handleKeyDown = null, placeholder = '', rows=1, validErrorMsg = ''}){
+function InputForm({label, name, value, onChange, onKeyDown = null, placeholder = '', rows=1, validErrorMsg = ''}){
     
     const style = validErrorMsg === '' 
         ?{} 
@@ -22,8 +20,8 @@ function InputForm({label, name, value, handleChange, handleKeyDown = null, plac
         style: style,
         name: name, 
         value: value,
-        onChange: handleChange, 
-        onKeyDown: handleKeyDown,
+        onChange: onChange, 
+        onKeyDown: onKeyDown,
         placeholder: placeholder,
     }
 
@@ -58,158 +56,23 @@ function SelectedTags({tags , handleDelete}){
 
 function Registration({}){
 
-    const [values, setValues] = useState({
-        name: '',
-        description: '',
-        price: '',
-        tag: '',
-        tags: [],
-    });
+    //입력값, 유효성 검사, 입력값을 다루는 함수 묶어서 커스텀 훅으로 만들었습니다.
+    //유효성 검사 커스텀 훅(요구사항)은 이 커스텀 훅 안에 있습니다.
+    const [
+        values,     //입력값
+        errors,     //유효성 메세지
+        isSubmitActive, //등록 버튼 활성화 여부
+        onChange,   //입력폼 onChange
+        addTag,     //태그 추가
+        deleteTag,  //태그 삭제
+        register    //상품 등록
+    ] = useRegisterInput(); 
 
-    //valid 오류 메세지를 관리하는 State. 겸사겸사 모든 항목이 valid 한지 체크에도 쓰고 있습니다;;
-    //이래도 괜찮은지 모르겠습니다;;
-    const [validErrorMsgs, setValidErrorMsgs] = useState({
-        name: '',
-        description: '',
-        price: '',
-        tag: '',
-    });
-
-    const nav = useNavigate();
-
-    const handleChange = (e) => {
-        const { name , value } = e.target;
-        const { message } = checkValidity(name, value);
-        
-        setValues((prevValues) => ({
-            ...prevValues,
-            [name]: value, //대괄호 표기법을 쓰는 이유는 문자열이기 때문
-        }));
-
-        setValidErrorMsgs((prevValues) => ({
-            ...prevValues,
-            [name]: message, 
-        }));
-    }
-
-    //태그 추가에만 대응하는 엔터 이벤트 핸들러... (이름을 뭐라고 지어야 할까요?)
-    const handleKeyDown = (e) => {
-        const { name , value } = e.target;
-
-        if (e.key === 'Enter' && checkValidity(name, value).result) {
-            const newTags = [...values.tags, values.tag];
-            setValues((prevValues) => ({
-                ...prevValues,
-                tag: '',
-                tags: [...new Set(newTags)] //중복 태그 제거
-            })); 
+    const handleKeyDown= (e) => {
+        if(e.key === 'Enter'){
+            addTag();
         }
-    };
-
-    const handleRegistration = async () => {
-        //등록 시 name, description, price 항목 valid 체크 후 오류메세지를 띄웁니다.
-        //(tags는 등록하지 않아도 통과)
-        setValidErrorMsgs((prevValues) => ({
-            ...prevValues,
-            name: checkValidity('name', values.name).message, 
-            description: checkValidity('description', values.description).message,
-            price: checkValidity('price', values.price).message,
-        }));
-
-        //하나라도 invalid하면 api 리퀘스트를 보내지 않습니다.
-        for(const key in validErrorMsgs){          
-            if(validErrorMsgs[key]!=='')return;
-        }
-        
-        //모두 vailid 하다면 리퀘스트를 보냅니다.
-        const RqBody = {
-            images: [
-                "https://example.com/..."
-            ],
-            tags: values.tags,
-            price: values.price,
-            description: values.description,
-            name: values.name,
-        }
-
-        const res = await productApi.createProduct(RqBody);
-        
-        //리퀘스트에 성공하면 상품 상세 사이트 이동
-        if(res){nav("/productdetail");}
-    }
-
-    const handleDeleteTag = (tag) => {
-        setValues((prevValues) => ({
-            ...prevValues,
-            tags: [...values.tags.filter((e)=> e!==tag)] //즉정 태그 제거
-        })); 
-    }
-
-
-    /*
-    오류 검사 후
-    {
-        result: 유효 여부,
-        message: '에러 메세지'
-    }
-    로 반환 합니다.
-    */
-    const checkValidity = (name, value) => {
-        switch(name){
-            case 'name':
-                if(value.length===0){return {
-                    result: false,
-                    message: '상품명을 입력해주세요'
-                }}
-                if(value.length>10){return {
-                    result: false,
-                    message: '10자 이내로 입력해주세요'
-                }}
-                return {
-                    result: true,
-                    message: ''
-                }
-            case 'description':
-                if(value.length<10){return {
-                    result: false,
-                    message: '10자 이상 입력해주세요'
-                }}
-                return {
-                    result: true,
-                    message: ''
-                }
-            case 'price':
-                if(value.length===0){return {
-                    result: false,
-                    message: '판매 가격을 입력해주세요'
-                }}
-                if(value.match(/\D+/g)){return {
-                    result: false,
-                    message: '숫자로 입력해주세요'
-                }}
-                return {
-                    result: true,
-                    message: ''
-                }
-            case 'tag':
-                //tag는 아무것도 입력 안할 시 valid하지는 않지만 오류 메세지를 출력하고 싶지 않아
-                //빈칸으로 두었습니다.
-                if(value.length===0){return {
-                    result: false,
-                    message: ''
-                }}
-                if(value.length>5){return {
-                    result: false,
-                    message: '5글자 이내로 입력해주세요'
-                }}
-                return {
-                    result: true,
-                    message: ''
-                }
-            case 'tags':
-                break;
-        }
-    }
+    }  
 
     //이번에 배운 사실: 리액트 JSX는 객체를 {}표현식에 넣어도 그대로 출력할 수 없다.
     return (
@@ -219,43 +82,43 @@ function Registration({}){
                     <div className='wrapper'>
                         <div className='headline'>
                             <h1>상품 등록하기</h1>
-                            <button className='button' onClick={handleRegistration}>등록</button>
+                            <button className='button' onClick={register} disabled={!isSubmitActive}>등록</button>
                         </div>
                         <InputForm 
                             label='상품명' 
                             name='name' 
                             value={values.name} 
-                            handleChange={handleChange}  
+                            onChange={onChange}  
                             placeholder='상품명을 입력해 주세요.'
-                            validErrorMsg={validErrorMsgs.name}                    
+                            validErrorMsg={errors.name}                    
                         />
                         <InputForm 
                             label='상품 소개' 
                             name='description' 
                             value={values.description} 
-                            handleChange={handleChange}
+                            onChange={onChange}
                             placeholder='상품소개를 입력해 주세요.'
                             rows={10}
-                            validErrorMsg={validErrorMsgs.description} 
+                            validErrorMsg={errors.description} 
                         />
                         <InputForm 
                             label='판매 가격' 
                             name='price' 
                             value={values.price} 
-                            handleChange={handleChange}
+                            onChange={onChange}
                             placeholder='판매 가격을 입력해 주세요.'
-                            validErrorMsg={validErrorMsgs.price} 
+                            validErrorMsg={errors.price} 
                         />
                         <InputForm 
                             label='태그' 
                             name='tag' 
                             value={values.tag} 
-                            handleChange={handleChange}
-                            handleKeyDown={handleKeyDown}
+                            onChange={onChange}
+                            onKeyDown={handleKeyDown}
                             placeholder='태그를 입력해 주세요.'
-                            validErrorMsg={validErrorMsgs.tag} 
+                            validErrorMsg={errors.tag} 
                         />
-                        <SelectedTags tags={values.tags} handleDelete={handleDeleteTag}/>
+                        <SelectedTags tags={values.tags} handleDelete={deleteTag}/>
                     </div>
                 </main> 
             <HomeFooter />
