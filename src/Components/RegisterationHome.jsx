95ea2f5a7e7d73f231d0d7497454fd30510548f2
chaewon127 <osobi621@gmail.com>
2025-08-24 20:00:50 +0900

    App.jsx 작성

--- src/Components/RegisterationHome.jsx
+++ src/Components/RegisterationHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header/Header";
 import ControllerBox from "./RegisterationPage/ControllerBox/ControllerBox";
-import inputBox from "./RegisterationPage/inputBoxes/inputBoxes";
+import InputBox from "./RegisterationPage/inputBoxes/inputBoxes";
 import Footer from "./Footer/Footer";
 import '../style/style.css';
 
@@ -16,7 +16,7 @@ function RegisterationHome() {
         </section>
 
         <section>
-          <inputBox />
+          <InputBox />
         </section>
       </main>
 
