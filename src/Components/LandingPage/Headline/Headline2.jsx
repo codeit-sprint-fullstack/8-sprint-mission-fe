c9edebc50525be38a90392aa9914f8f9286cc989
chaewon127 <osobi621@gmail.com>
2025-08-24 22:00:46 +0900

    Headline 추가

---
+++ src/Components/LandingPage/Headline/Headline2.jsx
@@ -0,0 +1,20 @@
+import style from './Headline.module.css';
+import HomeDownImg from '../../../images/home_bottom_img.svg';
+
+function Headline2() {
+  return (
+    <section className={style.headlineSectionBg}>
+      <div className={style.headlineSection}>
+        <div className={style.headlineTitle}>
+          <h1>
+            믿을 수 있는<br />
+            판다 마켓 중고 거래
+          </h1>
+        </div>
+        <img src={HomeDownImg} alt="HomeDownImg" />
+      </div>
+    </section>
+  )
+}
+
+export default Headline2;
