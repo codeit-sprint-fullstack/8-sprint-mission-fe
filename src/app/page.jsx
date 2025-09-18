c1b799f4871472beb0155eeab72849497bb02c73
chaewon127 <osobi621@gmail.com>
2025-09-18 10:14:13 +0900

    파일 구조

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Header />
+
+      <h1>랜딩 페이지 마이그레이션</h1>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
