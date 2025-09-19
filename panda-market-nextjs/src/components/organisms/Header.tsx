ed435569a125177eff17addc9f7f74bbd9a02825
seongEun95 <dsa02070@naver.com>
2025-09-19 13:22:37 +0900

    feat: 새로운 컴포넌트 및 스타일 추가, 레이아웃 구성 및 글로벌 스타일 초기화

---
+++ panda-market-nextjs/src/components/organisms/Header.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="border-b fixed top-0 z-10 w-full">
+      <div className="w-full max-w-[1260px] mx-auto px-[30px] py-[6px] flex items-center justify-between">
+        {/* 로고 */}
+        <div>
+          <ul className="flex items-center gap-4">
+            <li>
+              <Link href="/" className="block font-bold text-xl relative w-40">
+                <Image
+                  src="/header/logo.svg"
+                  alt="판다마켓 로고"
+                  priority
+                  width={153}
+                  height={32}
+                ></Image>
+              </Link>
+            </li>
+            <li>
+              <Link href="/free-board" className="text-md">
+                자유게시판
+              </Link>
+            </li>
+            <li>
+              <Link href="/items" className="text-md">
+                중고마켓
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* 버튼들 */}
+        <div className="flex gap-2">
+          <Button variant="default" asChild>
+            <Link href="/login">로그인</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
