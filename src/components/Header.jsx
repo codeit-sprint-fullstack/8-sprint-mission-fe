93a3caf386c815ec7ab54501397b4bf5957eafe4
MinJun916 <ed125248@gmail.com>
2025-09-22 16:12:18 +0900

    feat: create Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ import styles from "@/styles/components/Header.module.scss";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div>
+      <div className={styles.wrapper}>
         <Image src={brandLogo} alt="brandLogo" />
-        <div>
-          <div>자유게시판</div>
-          <div>중고마켓</div>
+        <div className={styles.link}>
+          <div className={styles.linkContent}>자유게시판</div>
+          <div className={styles.linkContent}>중고마켓</div>
         </div>
       </div>
-      <button>로그인</button>
+      <button className={styles.loginBtn}>로그인</button>
     </div>
   );
 };
