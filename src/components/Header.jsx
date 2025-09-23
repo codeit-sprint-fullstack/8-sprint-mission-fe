73b53692e9bf5b1c552d2332ffd739f95ecced2d
MinJun916 <ed125248@gmail.com>
2025-09-23 18:36:49 +0900

    refactor: update Header.jsx component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
-import brandLogo from "../../public/brandLogo.svg";
+import clsx from "clsx";
 import Button from "@/components/Button.jsx";
 import styles from "@/styles/components/Header.module.scss";
 
+import brandLogo from "../../public/brandLogo.svg";
+
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <div className={styles.header}>
       <div className={styles.wrapper}>
-        <Image src={brandLogo} alt="brandLogo" />
+        <Link href="/">
+          <Image src={brandLogo} alt="brandLogo" />
+        </Link>
         <div className={styles.link}>
-          <div className={styles.linkContent}>자유게시판</div>
-          <div className={styles.linkContent}>중고마켓</div>
+          <Link
+            href="/article"
+            className={clsx(
+              styles.linkContent,
+              pathname === "/article" && styles.active
+            )}
+          >
+            자유게시판
+          </Link>
+          <Link
+            href="/market"
+            className={clsx(
+              styles.linkContent,
+              pathname === "/market" && styles.active
+            )}
+          >
+            중고마켓
+          </Link>
         </div>
       </div>
-      <Button type="login" />
+      <Link href="/login">
+        <Button type="login" />
+      </Link>
     </div>
   );
 };
