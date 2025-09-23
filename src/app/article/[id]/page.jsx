bc128f85c78a31f6eb32a04195cff28310717499
MinJun916 <ed125248@gmail.com>
2025-09-24 01:28:07 +0900

    refactor: update Button.jsx props for custom elements

--- src/app/article/[id]/page.jsx
+++ src/app/article/[id]/page.jsx
@@ -1,17 +1,20 @@
+import Button from '@/components/Button';
 import CommentReplyCard from '@/components/CommentReplyCard';
+
 import styles from '@/styles/pages/DetailArticlePage.module.scss';
 
 const DetailArticlePage = () => {
   return (
     <div className={styles.detailArticlePage}>
       <div className={styles.pageWrapper}>
-        <div>
-          <div className={styles.detailText}>detail</div>
-          <div className={styles.submitComment}>Add Comment</div>
-          <div className={styles.commentReply}>Reply</div>
+        <div className={styles.detailText}>detail</div>
+        <div className={styles.commentWrapper}>
+          <div className={styles.addComment}>Add Comment</div>
+          <CommentReplyCard />
         </div>
-        <CommentReplyCard />
-        <button>button</button>
+        <div className={styles.buttonWrapper}>
+          <Button type="goBack" size="md" />
+        </div>
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,36 @@
+import Image from 'next/image';
+import clsx from 'clsx';
+import ic_back from '/public/icons/ic_back.svg';
+
 import styles from '@/styles/components/Button.module.scss';
 
-const Button = ({ type = 'login' }) => {
+/*
+  type: login | write | post | explore | goBack
+  size: sm | md | lg
+  bg: primary-100 | 200 | 300 | disabled
+*/
+const Button = ({ type = 'login', size = 'sm', bg = 'primary-100' }) => {
   const typeMap = {
     login: '로그인',
     write: '글쓰기',
     post: '등록',
+    explore: '구경하러 가기',
   };
 
-  return <button className={styles.button}>{typeMap[type]}</button>;
+  if (type === 'goBack') {
+    return (
+      <button className={clsx(styles.button, styles.goBack, styles[size], styles[bg])}>
+        <div className={styles.detail}>목록으로 돌아가기</div>
+        <Image src={ic_back} alt="ic_back" width={24} height={24} />
+      </button>
+    );
+  }
+
+  return (
+    <button className={clsx(styles.button, styles[type], styles[size], styles[bg])}>
+      {typeMap[type]}
+    </button>
+  );
 };
 
 export default Button;
