fa0933ece703ba04091fc83410c23542da0bac6b
chaewon127 <osobi621@gmail.com>
2025-09-25 16:46:11 +0900

    랜딩페이지 마이그레이션

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Desktop from "@/components/LandingPage/Desktop";
+import InfoCard from "@/components/LandingPage/InfoCard";
+import Deskbottom from "@/components/LandingPage/Deskbottom";
 
 const LandingPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
 
-      <main className="flex-1 flex flex-col items-stretch mx-auto mb-[200px] p-4 w-full max-w-[1200px]">
-        <h1 className="text-black">랜딩 페이지 마이그레이션</h1>
-        <p className="text-black">헤더에서 자유게시판 클릭 후 이동하기</p>
+      <Desktop />
+      <main className="flex-1 flex flex-col items-stretch mx-auto w-full max-w-[1200px]">
+        <InfoCard />
       </main>
+      <Deskbottom />
 
       <Footer />
     </div>
---
+++ src/components/LandingPage/Deskbottom.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+const Deskbottom = () => {
+  return (
+    <section className="relative flex justify-center w-full h-[540px] bg-[#CFE5FF]">
+      <div className="absolute bottom-0 flex items-center justify-center gap-7 max-w-[1200px] mx-auto">
+        <div className="flex flex-col justify-center items-start gap-8 pb-[60px]">
+          <h1 className="text-[40px] font-bold text-[#374151] leading-[140%]">
+            믿을 수 있는
+            <br />
+            판다 마켓 중고 거래
+          </h1>
+        </div>
+        <Image
+          src="/home_bottom_img.svg"
+          alt="HomeDown"
+          className="flex-shrink-0"
+          width={746}
+          height={397}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Deskbottom;
