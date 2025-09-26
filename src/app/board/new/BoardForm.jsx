6b0e605f2a0a3105b1922f805b6fe48a29e4f596
MyeongjuHong <hmjpearl@gmail.com>
2025-09-26 17:28:20 +0900

    feat: Implement BoardForm components

--- src/app/board/new/BoardForm.jsx
+++ src/app/board/new/BoardForm.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Button from "@/app/components/Button";
+import React from "react";
 
 const BoardForm = () => {
   return (
-    <div>BoardForm</div>
-  )
-}
+    <form className="flex flex-col gap-6 my-6">
+      <div className="flex justify-between items-center">
+        <div className="text-xl font-bold">게시글 쓰기</div>
+        <Button>등록</Button>
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className="text-lg font-bold">제목</div>
+        <input
+          placeholder="제목을 입력해주세요"
+          className="bg-gray-100 p-4 rounded-lg"
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className="text-lg font-bold">내용</div>
+        <textarea
+          placeholder="내용을 입력해주세요"
+          className="bg-gray-100 resize-none p-4 rounded-lg min-h-80"
+        />
+      </div>
+    </form>
+  );
+};
 
-export default BoardForm
+export default BoardForm;
