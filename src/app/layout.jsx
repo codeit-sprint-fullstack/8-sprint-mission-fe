0992ede3529cf1d16ccb106fba9f84add0673be3
MyeongjuHong <hmjpearl@gmail.com>
2025-09-26 17:22:29 +0900

    style: Add pretendard font style

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,26 @@
+import "./globals.css";
+import localFont from 'next/font/local';
 import Header from "@/components/Header";
+
+// 한글 전용 Pretendard GOV Variable
+const pretendardGOV = localFont({
+  src: "../../public/fonts/PretendardGOVVariable.woff2",
+  variable: "--font-pretendard-gov",
+  display: "swap", // 폰트 깜빡임 최소화
+});
+
+const pretendardStd = localFont({
+  src: "../../public/fonts/PretendardStdVariable.woff2",
+  variable: "--font-pretendard-std",
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html
+      lang="ko"
+      className={`${pretendardGOV.variable} ${pretendardStd.variable}`}
+    >
       <body>
         <Header />
         {children}
