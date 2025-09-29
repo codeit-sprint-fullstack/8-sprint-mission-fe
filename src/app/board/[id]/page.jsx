1d88f74c7ae31f1f25090796246078c570995db7
MyeongjuHong <hmjpearl@gmail.com>
2025-09-29 17:48:00 +0900

    feat: Refactor db.json and get comments

--- src/app/board/[id]/page.jsx
+++ src/app/board/[id]/page.jsx
@@ -3,13 +3,13 @@ import BoardDetail from "./BoardDetail";
 import Comments from "@/app/components/comments";
 
 const BoardDetailPage = async ({ params }) => {
-  const { id } = await params; //암튼
+  const { id } = await params; 
   const res = await fetch(`http://localhost:4000/posts/${id}`);
   const post = await res.json();
   return (
     <div>
       <BoardDetail data={post} />
-      <Comments data={post.comments} />
+      <Comments id={id} />
     </div>
   );
 };
--- src/app/components/comments/index.jsx
+++ src/app/components/comments/index.jsx
@@ -1,26 +1,64 @@
-// 'use client';
+"use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Item from "./_components/Item";
 import Button from "../Button";
 import Image from "next/image";
 
-const Comments = ({ data }) => {
-  // const [text, setText] = useState('');
+const Comments = ({ id }) => {
+  const [text, setText] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const getComments = async () => {
+    try {
+      const res = await fetch(`http://localhost:4000/comments?postId=${id}`);
+
+      if (!res.ok) throw new Error("get failed: ", res.statusText);
+      const data = await res.json();
+      setComments(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const postComment = async () => {
+    try {
+      const res = await fetch(`http://localhost:4000/posts/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      });
+
+      if (!res.ok) throw new Error("post failed: ", res.statusText);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const deleteComment = () => {};
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
   return (
     <div>
       <form className="flex flex-col">
         <label>댓글달기</label>
         <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           placeholder="댓글을 입력해주세요."
           className="bg-gray-100 resize-none p-4 rounded-lg mt-2 mb-4"
         />
         <div className="flex justify-end">
-          <Button>등록</Button>
+          <Button disabled={!text.trim()}>등록</Button>
         </div>
       </form>
-      {data ? (
-        data.map((comment) => <Item key={comment.id} data={comment} />)
+      {comments.length !== 0 ? (
+        comments.map((comment) => <Item key={comment.id} data={comment} />)
       ) : (
         <Image
           src="/reply-empty.svg"
