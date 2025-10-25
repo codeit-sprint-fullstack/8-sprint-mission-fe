1fcb1436254457cb53cb9e518d52aa2a19a6a00b
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-10-25 23:21:22 +0900

    회원가입/로그인 기능 구현 (프론트/백)

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,27 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import router from './api/index.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 dotenv.config();
 const corsOptions = {
   origin: ['http://localhost:3000'], //프론트엔드 개발 로컬 주소
+  credentials: true,
 };
 
+//app, 전역 미들웨어
 const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use(cookieParser());
 
+//라우터
 app.use(router);
+
+//에러 핸들러
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
