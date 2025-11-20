d16b18e72fc9d222197441ef2e96e797e79c783d
MinJun916 <ed125248@gmail.com>
2025-11-20 15:20:42 +0900

    feat: 랜딩페이지 마이그레이션 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,91 @@
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div className="w-full">
+      <section className="flex w-full justify-center bg-[#cfe5ff] px-[12vw] pt-50">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex flex-col justify-center gap-8">
+            <div className="text-secondary-700 text-[40px] leading-[56px] font-bold">
+              일상의 모든 물건을 거래해 보세요
+            </div>
+            <Link
+              href="/items"
+              className="bg-primary-100 mb-[60px] w-[357px] rounded-[40px] px-[124px] py-4 text-center text-xl leading-[32px] font-semibold whitespace-nowrap text-white"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              구경하러 가기
+            </Link>
+          </div>
+          <Image src="/img_home_top.svg" alt="topImage" width={746} height={340} />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </section>
+      <section className="w-full px-[10vw] py-[138px]">
+        <div className="flex justify-around gap-16">
+          <Image src="/section1_img.svg" alt="section1Img" width={588} height={444} />
+          <div className="flex flex-col gap-3">
+            <div className="text-primary-100 text-lg leading-[26px] font-bold">Hot Item</div>
+            <div className="flex flex-col justify-center gap-6">
+              <div className="text-secondary-700 text-[40px] leading-[56px] font-bold">
+                인기상품을 확인해 보세요
+              </div>
+              <div className="text-secondary-700 text-2xl leading-[32px]">
+                가장 HOT한 중고거래 물품을
+                <br />
+                판다 마켓에서 확인해 보세요
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
+      </section>
+      <section className="w-full px-[10vw] py-[138px]">
+        <div className="flex justify-around gap-16">
+          <div className="flex flex-col gap-3">
+            <div className="text-primary-100 text-lg leading-[26px] font-bold">Search</div>
+            <div className="flex flex-col justify-center gap-6">
+              <div className="text-secondary-700 text-[40px] leading-[56px] font-bold">
+                구매를 원하는 상품을 검색하세요
+              </div>
+              <div className="text-secondary-700 text-2xl leading-[32px]">
+                구매하고 싶은 물품은 검색해서
+                <br />
+                쉽게 찾아보세요
+              </div>
+            </div>
+          </div>
+          <Image src="/section2_img.svg" alt="section2Img" width={588} height={444} />
+        </div>
+      </section>
+      <section className="w-full px-[10vw] py-[138px]">
+        <div className="flex justify-around gap-16">
+          <Image src="/section3_img.svg" alt="section3Img" width={588} height={444} />
+          <div className="flex flex-col gap-3">
+            <div className="text-primary-100 text-lg leading-[26px] font-bold">Register</div>
+            <div className="flex flex-col justify-center gap-6">
+              <div className="text-secondary-700 text-[40px] leading-[56px] font-bold">
+                판매를 원하는 상품을 등록하세요
+              </div>
+              <div className="text-secondary-700 text-2xl leading-[32px]">
+                어떤 물건이든 판매하고 싶은 상품을
+                <br />
+                쉽게 등록하세요
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="flex w-full justify-center bg-[#cfe5ff] px-[12vw] pt-[143px]">
+        <div className="flex w-full justify-between">
+          <div className="mb-[60px] w-full text-[40px] leading-[56px] font-bold">
+            믿을 수 있는
+            <br />
+            판다마켓 중고 거래
+          </div>
+          <Image src="/img_home_bottom.svg" alt="bottomImg" width={746} height={397} />
+        </div>
+      </section>
     </div>
   );
-}
+};
+
+export default HomePage;
