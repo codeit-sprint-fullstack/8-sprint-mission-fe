a7ecfb688551f412d65ed0010eb8093e43ed0e9a
kwangche <163306559+kwangche@users.noreply.github.com>
2025-11-20 13:36:10 +0900

    feat: 타입스크립트 전환용 신규 next 프로젝트 세팅

--- src/app/(article)/article/[id]/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ArticleDetailPage from '@/components/pages/ArticleDetailPage';
-
-export default function ArticleDetail({ params }) {
-  const { id } = params || {};
-  return <ArticleDetailPage articleId={id} />;
-}
--- src/app/(items)/layout.jsx
+++
@@ -1,6 +0,0 @@
-import { DefaultLayout } from '@/components/Layout';
-import React from 'react';
-
-export default async function Layout({ children }) {
-  return <DefaultLayout>{children}</DefaultLayout>;
-}
