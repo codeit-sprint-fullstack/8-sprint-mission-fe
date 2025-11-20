fe4e4483613af979bff9999a36da2b95953b089e
MinJun916 <ed125248@gmail.com>
2025-11-20 17:56:11 +0900

    feat: 유저 정보 Zustand 전역 상태 관리 로직 추가 완료

---
+++ src/stores/useAuthStore.ts
@@ -0,0 +1,24 @@
+'use client';
+
+import { create } from 'zustand';
+
+interface User {
+  id: string;
+  email: string;
+  nickname: string;
+}
+
+export const useAuthStore = create<{
+  user: User | null;
+  isUserLoaded: boolean;
+  setUser: (user: User | null) => void;
+  clearUser: () => void;
+  markUserLoaded: () => void;
+}>((set) => ({
+  user: null,
+  isUserLoaded: false,
+
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
+  markUserLoaded: () => set({ isUserLoaded: true }),
+}));
